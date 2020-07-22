66fb8dae698e6e7096415c63649c1c9f6afef41a
Ahmet Mücahid Bozkurt <ahmetmb46@gmail.com>
2020-07-22 13:01:15 +0300

    Current location error fixed and splash screen added

---
+++ WeatherApp/Networking/EndPoints.swift
@@ -0,0 +1,24 @@
+//
+//  EndPoints.swift
+//  WeatherApp
+//
+//  Created by Ahmet Mücahid BOZKURT on 17.07.2020.
+//  Copyright © 2020 Ahmet Mucahid BOZKURT. All rights reserved.
+//
+
+import Foundation
+
+struct EndPoint {
+    
+    enum BaseUrl: String {
+        case prod = "http://api.openweathermap.org/data/2.5/weather"
+    }
+
+    static let apiKey = "6b31419b179bf6e22208bbc37e70ed0a"
+    static let latKey = "&lat="
+    static let lonKey = "&lon="
+    
+    //http://api.openweathermap.org/data/2.5/weather?lat=39&lon=37&appid=6b31419b179bf6e22208bbc37e70ed0a
+    static let getWeatherDetail = "\(Preferences.baseApi.rawValue)?appid=\(EndPoint.apiKey)&units=metric"
+
+}
---
+++ WeatherApp/ViewController/Splash/SplashVC.swift
@@ -0,0 +1,45 @@
+//
+//  SplashVC.swift
+//  WeatherApp
+//
+//  Created by Ahmet Mücahid BOZKURT on 22.07.2020.
+//  Copyright © 2020 Ahmet Mucahid BOZKURT. All rights reserved.
+//
+
+import Foundation
+import Reachability
+
+class SplashVC: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        checkNetwork()
+        
+    }
+    
+    private func checkNetwork() {
+        do {
+            let reachability = try Reachability()
+            let connection = reachability.connection
+            if connection == .unavailable || connection == .unavailable {
+                let alert = UIAlertController(title: "Error", message: "Check your internet connection.", preferredStyle: .alert)
+                self.present(alert, animated: true, completion: nil)
+            } else {
+                gotoMainPage()
+            }
+        } catch {
+            
+        }
+    }
+    
+    private func gotoMainPage() {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0, execute: {
+            if let vc = self.storyboard?.instantiateViewController(identifier: "MainPageVC") {
+                let nc = UINavigationController(rootViewController: vc)
+                UIApplication.shared.windows[0].rootViewController = nc
+            }
+            
+        })
+    }
+
+}
